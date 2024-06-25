51c882f8c73930c23652076560796736cb9a1f19
EviBera <beraevi@gmail.com>
2024-06-25 15:28:44 +0200

    Update models before DB creation

--- backend/AdoptAPet/Models/Advertisement.cs
+++ backend/AdoptAPet/Models/Advertisement.cs
@@ -3,6 +3,9 @@ namespace AdoptAPet.Models;
 public class Advertisement
 {
     public int Id { get; set; }
-    public Pet Pet { get; set; }
-    public List<Application> Applications { get; set; }
+    public int PetId { get; set; }
+    public Pet? Pet { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime ExpiresAt { get; set; }
+    public ICollection<Application> Applications { get; set; } = new List<Application>();
 }   
--- backend/AdoptAPet/Models/Pet.cs
+++ backend/AdoptAPet/Models/Pet.cs
@@ -5,12 +5,16 @@ public class Pet
 {
     [Key]
     public int Id { get; init; }
+    [MaxLength(30)]
     public string Name { get; set; } = String.Empty;
     public Species Species { get; init; }
     public DateTime Birth { get; set; } 
     public Gender Gender { get; init; }
     public bool IsNeutered { get; set; }
-    public string Description { get; set; }
+    [MaxLength(300)] public string Description { get; set; } = String.Empty;
     public User? Owner { get; set; }
-    public string PictureLink { get; set; }
+    [MaxLength(100)]
+    public string PictureLink { get; set; } = String.Empty;
+
+    public virtual ICollection<Advertisement> Advertisements { get; set; } = new List<Advertisement>();
 }
