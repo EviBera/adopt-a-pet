9a0076f6a4027280c4fe227f750a1010f838979f
EviBera <beraevi@gmail.com>
2024-06-25 15:29:56 +0200

    Add DBContext for connecting the DB

--- backend/AdoptAPet/Data/AppDbContext.cs
+++ backend/AdoptAPet/Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using AdoptAPet.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,4 +14,31 @@ public class AppDbContext : IdentityDbContext<User>
     
     public DbSet<Pet> Pets { get; set; }
     public DbSet<Advertisement> Advertisements { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+
+        // Configure one-to-many relationship between Pet and Advertisement
+        modelBuilder.Entity<Pet>()
+            .HasMany(p => p.Advertisements)
+            .WithOne(ad => ad.Pet)
+            .HasForeignKey(ad => ad.PetId);
+
+        // Configure one-to-many relationship between User and Application
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Applications)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserId)
+            .IsRequired();
+
+        // Configure one-to-many relationship between Advertisement and Application
+        modelBuilder.Entity<Advertisement>()
+            .HasMany(ad => ad.Applications)
+            .WithOne(a => a.Advertisement)
+            .HasForeignKey(a => a.AdvertisementId)
+            .IsRequired();
+    }
 }
--- backend/AdoptAPet/Program.cs
+++ backend/AdoptAPet/Program.cs
@@ -1,3 +1,6 @@
+using AdoptAPet.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
