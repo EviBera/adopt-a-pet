1d6089adc3d869eb84b93d6ab11a3fec235c8fa3
EviBera <beraevi@gmail.com>
2024-06-26 13:00:52 +0200

    Develop Pet repository to handle read and create operations

--- backend/AdoptAPet/Services/Repositories/IPetRepository.cs
+++ backend/AdoptAPet/Services/Repositories/IPetRepository.cs
@@ -1,12 +1,13 @@
+using AdoptAPet.DTOs.Pet;
 using AdoptAPet.Models;
 
 namespace AdoptAPet.Services.Repositories;
 
 public interface IPetRepository
 {
-    Task<IEnumerable<Pet>> GetAllAsync();
-    Task<Pet?> GetByIdAsync(int petId);
-    Task<Pet> CreateAsync(Pet pet);
+    Task<IEnumerable<PetDto>> GetAllAsync();
+    Task<PetDto?> GetByIdAsync(int petId);
+    Task<Pet> CreateAsync(CreatePetRequestDto newPet);
     Task<Pet> UpdateAsync(Pet pet);
     Task DeleteAsync(Pet pet);
 }
--- backend/AdoptAPet/Services/Repositories/PetRepository.cs
+++ backend/AdoptAPet/Services/Repositories/PetRepository.cs
@@ -1,22 +1,45 @@
+using System.Data;
+using AdoptAPet.Data;
+using AdoptAPet.DTOs.Pet;
+using AdoptAPet.Mappers;
 using AdoptAPet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdoptAPet.Services.Repositories;
 
 public class PetRepository : IPetRepository
 {
-    public Task<IEnumerable<Pet>> GetAllAsync()
+
+    private readonly AppDbContext _dbContext;
+
+    public PetRepository(AppDbContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
+    }
+    public async Task<IEnumerable<PetDto>> GetAllAsync()
+    {
+        var pets = await _dbContext.Pets.Select(p => p.ToPetDto()).ToListAsync();
+        return pets;
     }
 
-    public Task<Pet?> GetByIdAsync(int petId)
+    public async Task<PetDto?> GetByIdAsync(int petId)
     {
-        throw new NotImplementedException();
+        var pet = await _dbContext.Pets.FirstOrDefaultAsync(p => p.Id == petId);
+        if (pet != null)
+        {
+            return pet.ToPetDto();
+        }
+
+        throw new RowNotInTableException();
     }
 
-    public Task<Pet> CreateAsync(Pet pet)
+    public async Task<Pet> CreateAsync(CreatePetRequestDto newPet)
     {
-        throw new NotImplementedException();
+        var pet = newPet.ToPetFromCreatePetRequestDto();
+        await _dbContext.Pets.AddAsync(pet);
+        await _dbContext.SaveChangesAsync();
+
+        return pet;
     }
 
     public Task<Pet> UpdateAsync(Pet pet)
