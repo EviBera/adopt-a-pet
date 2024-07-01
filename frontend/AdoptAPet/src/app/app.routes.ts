e3b8e196cc2abf8ecf5e645ac2e6394ed881463d
EviBera <beraevi@gmail.com>
2024-07-01 14:08:50 +0200

    Add basic routing for existing components

--- frontend/AdoptAPet/src/app/app.routes.ts
+++ frontend/AdoptAPet/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdvertisementListComponent } from './advertisements/advertisement-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        title: 'Home'
+    },
+    {
+        path: 'adopt',
+        component: AdvertisementListComponent,
+        title: 'Adoptable pets'
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
