f09ecee06b7afd6b8184f6dcf0f76797600a9820
EviBera <beraevi@gmail.com>
2024-07-02 12:18:59 +0200

    Display adverisment ids

--- frontend/AdoptAPet/src/app/advertisements/advertisement-list.component.ts
+++ frontend/AdoptAPet/src/app/advertisements/advertisement-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { IAdvertisement } from '../models/advertisement.model';
+import { AdvertisementService } from './advertisement.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'aap-advertisement-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './advertisement-list.component.html',
   styleUrl: './advertisement-list.component.css'
 })
 export class AdvertisementListComponent {
+  advertisements: any
 
+  constructor(
+    private adSvc: AdvertisementService
+  ){}
+
+  ngOnInit(){
+    this.adSvc.getAdvertisementsOfAdoptablePets().subscribe((ads) => {
+      this.advertisements = ads;
+    })
+  }
 }
