c502d384f7f140f76b1ef6637df05b2ee03aee99
EviBera <beraevi@gmail.com>
2024-07-09 16:01:40 +0200

    Add component to display user's applications

---
+++ frontend/AdoptAPet/src/app/applications/applications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aap-applications',
+  standalone: true,
+  imports: [],
+  templateUrl: './applications.component.html',
+  styleUrl: './applications.component.css'
+})
+export class ApplicationsComponent {
+
+}
