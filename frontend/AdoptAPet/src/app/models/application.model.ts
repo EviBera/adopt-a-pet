d0e739b50accc8332342d615623dd6df06bbf9e5
EviBera <beraevi@gmail.com>
2024-07-16 16:10:11 +0200

    Expand application model, display more data of applications

--- frontend/AdoptAPet/src/app/models/application.model.ts
+++ frontend/AdoptAPet/src/app/models/application.model.ts
@@ -3,4 +3,12 @@ export interface IApplication {
     userId: string,
     advertisementId: number,
     isAccepted: boolean
+    advertisementExpiresAt: Date,
+    petName: string,
+    petSpecies: string,
+    petBirth: Date,
+    petGender: string,
+    petIsNeutered: boolean,
+    petDescription: string,
+    petPictureLink: string
 }
