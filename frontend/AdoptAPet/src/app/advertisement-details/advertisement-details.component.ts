40d607c48625af40af179fd759b419088eff8e21
EviBera <beraevi@gmail.com>
2024-08-01 15:27:29 +0200

    Replace testuser's id with the signed-in user's

--- frontend/AdoptAPet/src/app/advertisement-details/advertisement-details.component.ts
+++ frontend/AdoptAPet/src/app/advertisement-details/advertisement-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IAdvertisement } from '../models/advertisement.model';
 import { CommonModule } from '@angular/common';
+import { IUser } from '../models/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'aap-advertisement-details',
@@ -13,13 +15,24 @@ export class AdvertisementDetailsComponent {
   @Input() ad!: IAdvertisement;
   @Output() adopt = new EventEmitter();
 
+  private user:IUser | null = null;
+
+  constructor(private userSvc: UserService){
+    this.userSvc.getUser().subscribe({
+    next: (user) => { this.user = user}
+  })}
+
   adoptButtonClicked(ad: IAdvertisement){
     if(!this.hasApplication(ad))
       this.adopt.emit();
   }
 
   hasApplication(ad: IAdvertisement){
-    let app = ad.applications.filter(app => app.userId === '711704b1-035f-4f76-99d9-a1dece06c153');
+    if(!this.user)
+      return false;
+    else {
+      let app = ad.applications.filter(app => app.userId === this.user?.id);
     return app.length > 0;
+    }
   }
 }
