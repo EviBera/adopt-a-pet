9909a849befee7d12b72b471e332510ecaf204b8
EviBera <beraevi@gmail.com>
2024-08-14 10:27:10 +0200

    Generate new comonent for displaying and deleting ads

---
+++ frontend/AdoptAPet/src/app/management/ad-list/ad-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aap-ad-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './ad-list.component.html',
+  styleUrl: './ad-list.component.css'
+})
+export class AdListComponent {
+
+}
