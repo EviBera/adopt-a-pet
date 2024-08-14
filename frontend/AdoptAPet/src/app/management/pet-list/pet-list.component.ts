b61b8f7fc312746ec98e73e0c973ed633e873a0a
EviBera <beraevi@gmail.com>
2024-08-14 14:52:56 +0200

    Display list of pets

--- frontend/AdoptAPet/src/app/management/pet-list/pet-list.component.ts
+++ frontend/AdoptAPet/src/app/management/pet-list/pet-list.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { IPet, IUpdatePet } from '../../models/pet.model';
+import { ManagementService } from '../management.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'aap-pet-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pet-list.component.html',
   styleUrl: './pet-list.component.css'
 })
 export class PetListComponent {
+  pets: IPet[] = [];
+  showUpdateForm: boolean = false;
+  idToUpdate: number | null = null;
+  updatePetModel: IUpdatePet = {
+    id: 0,
+    name: '',
+    isNeutered: false,
+    description: '',
+    pictureLink: ''
+  }
 
+  constructor(private managementSvc: ManagementService){}
+
+  ngOnInit(){
+    this.managementSvc.getPets().subscribe((pets) => {
+      this.pets = pets;
+    })
+  }
+
+  sortPets(pets: IPet[]): IPet[]{
+    return pets.sort((a,b) => {return a.id - b.id});
+  }
+
+  get petList(): IPet[] {
+    return this.sortPets(this.pets);
+  }
+
+  updatePet(id: number){
+    console.log("Update: " + id);
+    this.showUpdateForm = true;
+    this.idToUpdate = id;
+    this.managementSvc.updatePet(id);
+  }
+
+  deletePet(id: number){
+    console.log("Delete: " + id);
+    this.managementSvc.deletePet(id);
+  }
 }
