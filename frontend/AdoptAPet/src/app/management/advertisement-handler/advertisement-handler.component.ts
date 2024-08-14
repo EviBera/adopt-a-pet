97318f8b6532b3395293fc9c3d36ba6ea6ae2839
EviBera <beraevi@gmail.com>
2024-08-14 10:28:39 +0200

    Nest new components into ad handler

--- frontend/AdoptAPet/src/app/management/advertisement-handler/advertisement-handler.component.ts
+++ frontend/AdoptAPet/src/app/management/advertisement-handler/advertisement-handler.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AdListComponent } from '../ad-list/ad-list.component';
+import { PetListComponent } from '../pet-list/pet-list.component';
 
 @Component({
   selector: 'aap-advertisement-handler',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AdListComponent, PetListComponent],
   templateUrl: './advertisement-handler.component.html',
   styleUrl: './advertisement-handler.component.css'
 })
 export class AdvertisementHandlerComponent {
+  showPets: boolean = false;
+  showAds: boolean = false;
+
+  constructor(){}
+
+  setPetVisibility(){
+    this.showPets = !this.showPets;
+  }
+
+  setAdvertisementVisibiliy(){
+    this.showAds = !this.showAds;
+  }
 
 }
