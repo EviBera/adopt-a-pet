8588f11d371388e9fd6b031f2f0756d10ab13436
EviBera <beraevi@gmail.com>
2024-08-14 10:27:51 +0200

    Generate new component for displaying, deleting and updating pets

---
+++ frontend/AdoptAPet/src/app/management/pet-list/pet-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aap-pet-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet-list.component.html',
+  styleUrl: './pet-list.component.css'
+})
+export class PetListComponent {
+
+}
