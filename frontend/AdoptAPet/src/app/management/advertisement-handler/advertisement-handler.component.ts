51c6891159818936963c5b8a1aa60dcfd8e5089c
EviBera <beraevi@gmail.com>
2024-08-15 13:59:06 +0200

    Add component for creating new pets

--- frontend/AdoptAPet/src/app/management/advertisement-handler/advertisement-handler.component.ts
+++ frontend/AdoptAPet/src/app/management/advertisement-handler/advertisement-handler.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AdListComponent } from '../ad-list/ad-list.component';
 import { PetListComponent } from '../pet-list/pet-list.component';
+import { CreatePetComponent } from '../create-pet/create-pet.component';
 
 @Component({
   selector: 'aap-advertisement-handler',
   standalone: true,
-  imports: [CommonModule, AdListComponent, PetListComponent],
+  imports: [CommonModule, AdListComponent, PetListComponent, CreatePetComponent],
   templateUrl: './advertisement-handler.component.html',
   styleUrl: './advertisement-handler.component.css'
 })
 export class AdvertisementHandlerComponent {
   showPets: boolean = false;
   showAds: boolean = false;
+  showPetGenerator: boolean = false;
 
   constructor(){}
 
@@ -22,6 +24,10 @@ export class AdvertisementHandlerComponent {
 
   setAdvertisementVisibiliy(){
     this.showAds = !this.showAds;
+  }
+
+  setPetGeneratorVisibility(){
+    this.showPetGenerator = !this.showPetGenerator;
   }
 
 }
