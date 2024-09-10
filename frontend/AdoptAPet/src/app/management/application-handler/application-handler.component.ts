4704556971ae60fc2e0f7bd8293ba9accf7542a7
EviBera <beraevi@gmail.com>
2024-09-10 15:15:07 +0200

    Display applications of an ad, catch id of selected application

--- frontend/AdoptAPet/src/app/management/application-handler/application-handler.component.ts
+++ frontend/AdoptAPet/src/app/management/application-handler/application-handler.component.ts
@@ -3,23 +3,26 @@ import { ActivatedRoute } from '@angular/router';
 import { IAdvertisement } from '../../models/advertisement.model';
 import { ManagementService } from '../management.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { IApplication } from '../../models/application.model';
 
 @Component({
   selector: 'aap-application-handler',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './application-handler.component.html',
   styleUrl: './application-handler.component.css'
 })
 export class ApplicationHandlerComponent {
   advertisementId: string | null = '';
   advertisement: IAdvertisement | null = null;
+  applicationId!: number;
+  selectedUserId: string | null = '';
 
   constructor(private route: ActivatedRoute, private managementSvc: ManagementService) {}
 
   ngOnInit() {
     this.advertisementId = this.route.snapshot.paramMap.get('advertisementId');
-    console.log("param: " + this.advertisementId);
 
     if(this.advertisementId !== null){
       this.fetchAd(this.advertisementId);
@@ -30,5 +33,13 @@ export class ApplicationHandlerComponent {
     this.managementSvc.getAdById(advertisementId).subscribe((advertisement) => {
       this.advertisement = advertisement;
     })
+  }
+
+  changeSelectedoption(application: IApplication){
+    this.applicationId = application.id;
+  }
+
+  selectOwner(form: NgForm){
+    console.log(this.applicationId);
   }
 }
